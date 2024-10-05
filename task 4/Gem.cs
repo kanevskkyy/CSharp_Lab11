5acb4b133ea8535b1249d671479ef1c720f62cf4
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-05 14:44:53 +0300

    create class gem

---
+++ task 4/Gem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    internal class Gem
+    {
+            public int Strength { get; }
+            public int Agility { get; }
+            public int Vitality { get; }
+
+        public Gem(int strength, int agility, int vitality, string clarity)
+        {
+            int modifier = 0;
+            if (clarity == "Chipped") modifier = 1;
+            else if (clarity == "Regular") modifier = 2;
+            else if (clarity == "Perfect") modifier = 3;
+            else if (clarity == "Flawless") modifier = 10;
+            Strength = strength + modifier;
+            Agility = agility + modifier;
+            Vitality = vitality + modifier;
+        }
+    }
+}
---
+++ task 4/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    public enum Rarity
+    {
+        Common = 1,
+        Uncommon = 2,
+        Rare = 3,
+        Epic = 5
+    }
+
+    internal class Weapon
+    {
+        public string Name { get; set; }
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+        public int Sockets { get; set; }
+
+        private Rarity rarity;
+
+
+
+    }
+}
